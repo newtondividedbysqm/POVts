47aba342ae49537e3045beb9e6751ad26aecd1e4
Pascal <27069811+newtondividedbysqm@users.noreply.github.com>
2025-07-02 11:29:43 +0200

    feat: add ip-address validation to string schemata

--- index.ts
+++ index.ts
@@ -276,6 +276,7 @@ export class StringSchema extends Schema<string> {
   private _IBAN: boolean = false;
   private _BIC: boolean = false;
   private _postal: boolean = false;
+  private _ipVersion: false | 4 | 6 = false;
 
   /**
    * Coerces the value into a String using the JS-built-in String() function
@@ -434,6 +435,10 @@ export class StringSchema extends Schema<string> {
     return this;
   }
 
+  ip(version: 4 | 6 = 4) {
+    this._ipVersion = version;
+    return this;
+  }
   /*
 ........######..########.########..####.##....##..######..
 .......##....##....##....##.....##..##..###...##.##....##.
@@ -577,6 +582,16 @@ export class StringSchema extends Schema<string> {
         success: false,
         error: [`must be a valid BIC, given was ${value}`],
       });
+    }
+
+    if (this._ipVersion) {
+      const ipRegex = this._ipVersion === 4 ? IPv4AddressRegExp : IPv6AddressRegExp;
+      if (!ipRegex.test(value)) {
+        return this.postValidationCheck({
+          success: false,
+          error: [`must be a valid IPv${this._ipVersion} address, given was ${givenValue}`],
+        });
+      }
     }
 
     return { success: true, value: value };
@@ -1721,3 +1736,21 @@ const ibanRegexThroughCountryCode = {
 
 // from https://github.com/validatorjs/validator.js/blob/master/src/lib/isBIC.js
 const isBICReg = /^[A-Za-z]{6}[A-Za-z0-9]{2}([A-Za-z0-9]{3})?$/;
+
+
+// from https://github.com/validatorjs/validator.js/blob/master/src/lib/isIP.js
+const IPv4SegmentFormat = '(?:[0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])';
+const IPv4AddressFormat = `(${IPv4SegmentFormat}[.]){3}${IPv4SegmentFormat}`;
+const IPv4AddressRegExp = new RegExp(`^${IPv4AddressFormat}$`);
+
+const IPv6SegmentFormat = '(?:[0-9a-fA-F]{1,4})';
+const IPv6AddressRegExp = new RegExp('^(' +
+  `(?:${IPv6SegmentFormat}:){7}(?:${IPv6SegmentFormat}|:)|` +
+  `(?:${IPv6SegmentFormat}:){6}(?:${IPv4AddressFormat}|:${IPv6SegmentFormat}|:)|` +
+  `(?:${IPv6SegmentFormat}:){5}(?::${IPv4AddressFormat}|(:${IPv6SegmentFormat}){1,2}|:)|` +
+  `(?:${IPv6SegmentFormat}:){4}(?:(:${IPv6SegmentFormat}){0,1}:${IPv4AddressFormat}|(:${IPv6SegmentFormat}){1,3}|:)|` +
+  `(?:${IPv6SegmentFormat}:){3}(?:(:${IPv6SegmentFormat}){0,2}:${IPv4AddressFormat}|(:${IPv6SegmentFormat}){1,4}|:)|` +
+  `(?:${IPv6SegmentFormat}:){2}(?:(:${IPv6SegmentFormat}){0,3}:${IPv4AddressFormat}|(:${IPv6SegmentFormat}){1,5}|:)|` +
+  `(?:${IPv6SegmentFormat}:){1}(?:(:${IPv6SegmentFormat}){0,4}:${IPv4AddressFormat}|(:${IPv6SegmentFormat}){1,6}|:)|` +
+  `(?::((?::${IPv6SegmentFormat}){0,5}:${IPv4AddressFormat}|(?::${IPv6SegmentFormat}){1,7}|:))` +
+  ')(%[0-9a-zA-Z.]{1,})?$');
